65995883c4122f915bb1de2edeb91490c6fe46bb
Mike Angstadt <mangstad@umich.edu>
2017-10-01 15:06:16 -0400

    cleaned up help

--- fsto3d.sh
+++ fsto3d.sh
@@ -6,22 +6,28 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ## Let's do some admin work to find out the variables to be used here
 BOLD='\e[1;31m'         # Bold Red
 REV='\e[1;32m'       # Bold Green
+OFF='\e[1;37m'
 
 #Help function
 function HELP {
-    echo -e "${REV}Basic usage:${OFF} ${BOLD}$SCRIPT -d helloworld ${OFF}"\\n
-    echo -e "${REV}The following switches are recognized. $OFF "
-    echo -e "${REV}-p ${OFF}  --Sets the environment to use for installing python ${OFF}. Default is ${BOLD} /usr/bin ${OFF}"
-    echo -e "${REV}-d ${OFF}  --Sets the directory whose virtualenv is to be setup. Default is ${BOLD} local folder (.) ${OFF}"
-    echo -e "${REV}-v ${OFF}  --Sets the python version that you want to install. Default is ${BOLD} 2.7 ${OFF}"
-    echo -e "${REV}-h${OFF}  --Displays this help message. No further functions are performed."\\n
-    echo -e "Example: ${BOLD}$SCRIPT -d helloworld -p /opt/py27env/bin -v 2.7 ${OFF}"\\n
+    echo -e "${REV}Basic usage:${OFF} ${BOLD}$SCRIPT -s /path/to/freesurfer/subject -u [pial|white|inflated] -p annotation${OFF}"\\n
+    echo -e "${REV}Required Options${OFF}"
+    echo -e "${REV}-s ${OFF}  Sets the path to a freesurfer subject${BOLD}${OFF}"\\n
+    echo -e "${REV}Other options${OFF}"
+    echo -e "${REV}-u ${OFF}  Which surface do you want to use. Default is ${BOLD}${surf}${OFF}"
+    echo -e "${REV}-p ${OFF}  Which annotation file to use. Default is ${BOLD}${parc}${OFF}"
+    echo -e "${REV}-b ${OFF}  Hemisphere to process (lh or rh). Default is ${BOLD}${hemi}${OFF}"
+    echo -e "${REV}-m ${OFF}  Meshlab script file to apply to the mesh. Default is \\n     ${BOLD}${mlx}${OFF}"
+    echo -e "${REV}-n ${OFF}  String to include in the name for the final files. Default is ${BOLD}${name}${OFF}"
+    echo -e "${REV}-h ${OFF}  Show this help information."
     exit 1
 }
 
-hemi="b"
+hemi="both"
 mlx="${DIR}/meshlab/simplify_clean_vertex.mlx"
 name="color"
+surf="pial"
+parc="aparc"
 
 while getopts s:b:u:p:m:n:h FLAG; do
     case $FLAG in
@@ -59,7 +65,7 @@ mkdir -p ${path}/3dp
 LOG=${path}/3dp/fsto3d.log
 
 #convert surface(s) to ASCII format
-if [ $hemi = "b" ];
+if [ $hemi = "both" ];
 then
     mris_convert ${path}/surf/lh.${surf} ${path}/3dp/lh.${surf}.asc >> ${LOG}
     mris_convert ${path}/surf/rh.${surf} ${path}/3dp/rh.${surf}.asc >> ${LOG}
@@ -71,7 +77,7 @@ fi
 python ${DIR}/fsto3d.py -a ${path}/label/${parc}.annot.ctab ${path}/3dp/${parc}.24bit.ctab >> ${LOG}
 
 #convert surface(s) to ASCII format with color integer values
-if [ $hemi = "b" ];
+if [ $hemi = "both" ];
 then
     mris_convert --annot ${path}/label/lh.${parc}.annot --parcstats ${path}/3dp/${parc}.24bit.ctab ${path}/surf/lh.${surf} ${path}/3dp/lh.${surf}.${parc}.asc >> ${LOG}
     mris_convert --annot ${path}/label/rh.${parc}.annot --parcstats ${path}/3dp/${parc}.24bit.ctab ${path}/surf/rh.${surf} ${path}/3dp/rh.${surf}.${parc}.asc >> ${LOG}
@@ -80,7 +86,7 @@ else
 fi
 
 #paste together the two ASCII files to get both vertex/face info and color info
-if [ $hemi = "b" ];
+if [ $hemi = "both" ];
 then
     python ${DIR}/fsto3d.py -b ${path}/3dp/lh.${surf}.asc ${path}/3dp/lh.${surf}.${parc}.asc ${path}/3dp/lh.${surf}.${parc}.color.asc >> ${LOG}
     python ${DIR}/fsto3d.py -b ${path}/3dp/rh.${surf}.asc ${path}/3dp/rh.${surf}.${parc}.asc ${path}/3dp/rh.${surf}.${parc}.color.asc >> ${LOG}
@@ -89,7 +95,7 @@ else
 fi
 
 #convert to vertex-colored obj file
-if [ $hemi = "b" ];
+if [ $hemi = "both" ];
 then
     ${DIR}/srf2obj_color ${path}/3dp/lh.${surf}.${parc}.color.asc > ${path}/3dp/lh.${surf}.${parc}.${name}.obj 
     ${DIR}/srf2obj_color ${path}/3dp/rh.${surf}.${parc}.color.asc > ${path}/3dp/rh.${surf}.${parc}.${name}.obj 
@@ -98,7 +104,7 @@ else
 fi
 
 #now run meshlabserver script to reduce complexity, fix some potential issues, and convert vertex coloring to texture map
-if [ $hemi = "b" ];
+if [ $hemi = "both" ];
 then
     cp ${mlx} ${path}/3dp/script.mlx
     sed -i "s/TEMP_TEXTURE/lh.${surf}.${parc}/" ${path}/3dp/script.mlx
